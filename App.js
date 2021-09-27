af9277b43199ffbdb49c37d60b1c2de1488ca2f6
Anish Dhungel <adhungel@ucdavis.edu>
2021-09-27 13:51:49 -0700

    create boilerplate project for immersive exp

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import MainNavigator from './src/navigations/MainNavigator';
+import { enableScreens } from 'react-native-screens';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
+enableScreens();
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    light: require('./assets/fonts/SF-Pro-Text-Light.ttf'),
+    medium: require('./assets/fonts/SF-Pro-Text-Medium.ttf'),
+    regular: require('./assets/fonts/SF-Pro-Text-Regular.ttf'),
+    semiBold: require('./assets/fonts/SF-Pro-Text-Semibold.ttf'),
+    bold: require('./assets/fonts/SF-Pro-Text-Bold.ttf')
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MainNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
