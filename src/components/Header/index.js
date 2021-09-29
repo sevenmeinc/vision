f3572ae790615722810e6b5e5b0b5eb0a2603cab
datnguyenner <dat.nguyen@seven.me>
2021-09-29 14:59:01 -0400

    Add reflection home page

---
+++ src/components/Header/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+const Header = (props) => {
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingTop: 16,
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        backgroundColor: '#fff'
+      }}>
+      <Text
+        style={{
+          fontSize: 14,
+          color: '#666666',
+          fontWeight: '600',
+          textAlign: 'center',
+          letterSpacing: '-.01'
+        }}>
+        Reflection activity
+      </Text>
+      <TouchableOpacity onPress={() => props.navigation.goBack()}>
+        <Text
+          style={{
+            fontSize: 14,
+            color: '#00968A',
+            fontWeight: '500',
+            textAlign: 'center',
+            letterSpacing: '-.01'
+          }}>
+          Close
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default Header
--- src/navigations/Flow2Navigator/index.js
+++ src/navigations/Flow2Navigator/index.js
@@ -3,13 +3,20 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { modalOptions } from '../utils'
 import Flow2 from '../../screens/Flow2'
 import ReflectionHome from '../../screens/ReflectionHome'
+import Header from '../../components/Header'
 
 const Stack = createStackNavigator()
 const Flow2Navigator = () => {
   return (
     <Stack.Navigator {...modalOptions}>
       <Stack.Screen name="Flow 2" component={Flow2} />
-      <Stack.Screen name="reflectionHome" component={ReflectionHome} />
+      <Stack.Screen
+        name="reflectionHome"
+        component={ReflectionHome}
+        options={({ navigation }) => ({
+          header: () => <Header navigation={navigation} />
+        })}
+      />
     </Stack.Navigator>
   )
 }
