114d8aacee2ace967e5b07cdc17c47faca231885
Anish Dhungel <adhungel@ucdavis.edu>
2021-09-29 12:03:45 -0700

    WIP

--- src/navigations/MainNavigator/index.js
+++ src/navigations/MainNavigator/index.js
@@ -3,12 +3,13 @@ import { createStackNavigator } from '@react-navigation/stack'
 import FlowScreen from '../../screens/FlowScreen'
 import Flow1 from '../../screens/Flow1'
 import Flow2Navigator from '../Flow2Navigator'
+import Flow1Navigator from '../Flow1Navigator'
 
 const Stack = createStackNavigator()
 const MainNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Flows" component={FlowScreen} />
+      <Stack.Screen name="Flows" component={Flow1Navigator} />
       <Stack.Screen name="flow1" component={Flow1} />
       <Stack.Screen
         name="Flow 2"
