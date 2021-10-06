cc738e93173e55ab0dfc017f9ae6bd3a04200cda
rob-seven <rob@seven.me>
2021-10-06 15:19:16 -0600

    update home to have all buttons

--- src/screens/FlowScreen.js
+++ src/screens/FlowScreen.js
@@ -22,7 +22,10 @@ const FlowScreen = (props) => {
         onPress={() => props.navigation.navigate('positiveThinking')}>
         Positive Thinking Flow 1
       </Button>
-      <Button onPress={() => props.navigation.navigate('SucHab01')}>
+      <Button
+        style={{ marginTop: 8 }}
+        mode="outlined"
+        onPress={() => props.navigation.navigate('SucHab01')}>
         Successful Habits 1
       </Button>
     </View>
